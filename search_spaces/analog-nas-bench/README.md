9b595bf664489128cae850a04bfca49363e307cc
Hadjer Benmeziane HAB@zurich.ibm.com <analognas@cccxl009.pok.ibm.com>
2022-06-07 05:23:08 -0400

    add analog-nas-bench

---
+++ search_spaces/analog-nas-bench/README.md
@@ -0,0 +1,11 @@
+This repository contains the code used for generating and interacting with the Analog-NAS-Bench. 
+The complete search space contains 32M architectures. 
+We trained a surrogate model on 864 architectures. 
+
+Each network is trained and evaluated multiple times on CIFAR-10, VWW, Google Speech Command.
+We present the metrics in a queriable API. If the architecture wasn't trained, the API will output the surrogate model's prediction. 
+You can force the API to only iterate over trained architectures with ```dataset_only=True```. 
+
+## Macro-architecture 
+![resnet-like architecture](./images/ss.PNG)
+
---
+++ search_spaces/analog-nas-bench/bench/macro_architecture.py
@@ -0,0 +1,219 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import operator
+import functools
+
+class ResidualBranch(nn.Module):
+    def __init__(self, in_channels, out_channels, filter_size, stride, branch_index):
+        super(ResidualBranch, self).__init__()
+
+        self.residual_branch = nn.Sequential()
+
+        self.residual_branch.add_module('Branch_{}:ReLU_1'.format(branch_index),
+                                        nn.ReLU(inplace=False))
+        self.residual_branch.add_module('Branch_{}:Conv_1'.format(branch_index),
+                                        nn.Conv2d(in_channels,
+                                                  out_channels,
+                                                  kernel_size=filter_size,
+                                                  stride=stride,
+                                                  padding=round(filter_size / 3),
+                                                  bias=False))
+        self.residual_branch.add_module('Branch_{}:BN_1'.format(branch_index),
+                                        nn.BatchNorm2d(out_channels))
+        self.residual_branch.add_module('Branch_{}:ReLU_2'.format(branch_index),
+                                        nn.ReLU(inplace=False))
+        self.residual_branch.add_module('Branch_{}:Conv_2'.format(branch_index),
+                                        nn.Conv2d(out_channels,
+                                                  out_channels,
+                                                  kernel_size=filter_size,
+                                                  stride=1,
+                                                  padding=round(filter_size / 3),
+                                                  bias=False))
+        self.residual_branch.add_module('Branch_{}:BN_2'.format(branch_index),
+                                        nn.BatchNorm2d(out_channels))
+
+    def forward(self, x):
+        return self.residual_branch(x)
+
+class SkipConnection(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(SkipConnection, self).__init__()
+
+        self.s1 = nn.Sequential()
+        self.s1.add_module('Skip_1_AvgPool',
+                           nn.AvgPool2d(1, stride=stride))
+        self.s1.add_module('Skip_1_Conv',
+                           nn.Conv2d(in_channels,
+                                     int(out_channels / 2),
+                                     kernel_size=1,
+                                     stride=1,
+                                     padding=0,
+                                     bias=False))
+
+        self.s2 = nn.Sequential()
+        self.s2.add_module('Skip_2_AvgPool',
+                           nn.AvgPool2d(1, stride=stride))
+        self.s2.add_module('Skip_2_Conv',
+                           nn.Conv2d(in_channels,
+                                     int(out_channels / 2) if out_channels % 2 == 0 else int(out_channels / 2) + 1,
+                                     kernel_size=1,
+                                     stride=1,
+                                     padding=0,
+                                     bias=False))
+
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        out1 = F.relu(x, inplace=False)
+        out1 = self.s1(out1)
+
+        out2 = F.pad(x[:, :, 1:, 1:], (0, 1, 0, 1))
+        out2 = self.s2(out2)
+
+        out = torch.cat([out1, out2], dim=1)
+        out = self.batch_norm(out)
+
+        return out
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, n_input_plane, n_output_plane, filter_size, res_branches, stride):
+        super(BasicBlock, self).__init__()
+
+        self.branches = nn.ModuleList([ResidualBranch(n_input_plane, n_output_plane, filter_size, stride, branch + 1) for branch in range(res_branches)])
+
+        self.skip = nn.Sequential()
+        if n_input_plane != n_output_plane or stride != 1:
+            self.skip.add_module('Skip_connection',
+                                 SkipConnection(n_input_plane, n_output_plane, stride))
+                                 
+
+    def forward(self, x):
+        out = sum([self.branches[i](x) for i in range(len(self.branches))])
+        return out + self.skip(x)
+
+
+class ResidualGroup(nn.Module):
+    def __init__(self, block, n_input_plane, n_output_plane, n_blocks, filter_size, res_branches, stride):
+        super(ResidualGroup, self).__init__()
+        self.group = nn.Sequential()
+        self.n_blocks = n_blocks
+
+        self.group.add_module('Block_1',
+                              block(n_input_plane,
+                                    n_output_plane,
+                                    filter_size,
+                                    res_branches,
+                                    stride=1))
+
+        # The following residual block do not perform any downsampling (stride=1)
+        for block_index in range(2, n_blocks + 1):
+            block_name = 'Block_{}'.format(block_index)
+            self.group.add_module(block_name,
+                                  block(n_output_plane,
+                                        n_output_plane,
+                                        filter_size,
+                                        res_branches,
+                                        stride=1))
+
+    def forward(self, x):
+        return self.group(x)
+
+
+class Network(nn.Module):
+    def __init__(self, config, input_size=(3,100,100), num_classes=2):
+        super(Network, self).__init__()
+
+        self.M = config["M"]
+        self.residual_blocks = {'Group_1': config["R1"],
+                                'Group_2': config["R2"],
+                                'Group_3': config["R3"],
+                                'Group_4': config["R4"],
+                                'Group_5': config["R5"]
+                                }
+ 
+        self.widen_factors = {'Group_1': config["widenfact1"],
+                              'Group_2': config["widenfact2"],
+                              'Group_3': config["widenfact3"],
+                              'Group_4': config["widenfact4"],
+                              'Group_5': config["widenfact5"]
+                              }
+
+        self.res_branches = {'Group_1': config["B1"],
+                             'Group_2': config["B2"],
+                             'Group_3': config["B3"],
+                             'Group_4': config["B4"],
+                             'Group_5': config["B5"]
+                             }
+
+        self.conv_blocks =  {'Group_1': config["convblock1"],
+                             'Group_2': config["convblock2"],
+                             'Group_3': config["convblock3"],
+                             'Group_4': config["convblock4"],
+                             'Group_5': config["convblock5"]
+                             }
+        
+        self.filters_size = {'Group_1': 3,
+                             'Group_2': 3,
+                             'Group_3': 3,
+                             'Group_4': 3,
+                             'Group_5': 3
+                             }
+        
+        self.model = nn.Sequential()
+        block = BasicBlock
+        self.blocks = nn.Sequential()
+        self.blocks.add_module('Conv_0',
+                              nn.Conv2d(3,
+                                        config["out_channel0"],
+                                        kernel_size=7,
+                                        stride=1,
+                                        padding=1,
+                                        bias=False))
+        
+        self.blocks.add_module('BN_0',
+                              nn.BatchNorm2d(config["out_channel0"]))
+
+        feature_maps_in = int(round(config["out_channel0"] // self.widen_factors['Group_1']))
+        
+        self.blocks.add_module('Group_1',
+                              ResidualGroup(block, 
+                                            config["out_channel0"], 
+                                            feature_maps_in, 
+                                            self.residual_blocks['Group_1'], 
+                                            self.filters_size['Group_1'],
+                                            self.res_branches['Group_1'],
+                                            1))
+        feature_maps_out = feature_maps_in
+        #feature_maps_out = int(round(feature_maps_in // self.widen_factors['Group_2']))
+        for m in range(2, self.M + 1):
+            feature_maps_out = int(round(feature_maps_in // self.widen_factors['Group_{}'.format(m)]))
+            self.blocks.add_module('Group_{}'.format(m),
+                                  ResidualGroup(block, 
+                                                feature_maps_in, 
+                                                feature_maps_out, 
+                                                self.residual_blocks['Group_{}'.format(m)],
+                                                self.filters_size['Group_{}'.format(m)],
+                                                self.res_branches['Group_{}'.format(m)],
+                                                2 if m in (self.M, self.M - 1) else 1))
+            feature_maps_in = feature_maps_out
+            
+        
+        self.feature_maps_out = feature_maps_out
+        self.blocks.add_module('ReLU_0',
+                              nn.ReLU(inplace=True))
+        
+        self.model.add_module("Main_blocks" ,self.blocks)
+        self.fc_len  = functools.reduce(operator.mul, list(F.avg_pool2d(self.blocks(torch.rand(1, *input_size)), self.blocks(torch.rand(1, *input_size)).size()[3]).shape))
+        self.fc = nn.Linear(self.fc_len, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = F.avg_pool2d(x, x.size()[3])
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return F.softmax(x)
+    
