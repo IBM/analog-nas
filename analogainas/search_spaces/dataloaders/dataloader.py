e10e40c333527f01f43976397add9d02d9bbb56c
Vishwa Gopal <vishgoki@gmail.com>
2024-05-07 14:03:35 -0400

    Update dataloader.py
    Added dataloader for UNet data

--- analogainas/search_spaces/dataloaders/dataloader.py
+++ analogainas/search_spaces/dataloaders/dataloader.py
@@ -7,6 +7,107 @@ import importlib.util
 pyvww = importlib.util.find_spec("pyvww")
 found = pyvww is not None
 
+import monai
+import matplotlib
+from monai.utils import first, set_determinism
+from monai.transforms import (
+    AsDiscrete,
+    AsDiscreted,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    SaveImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+    Invertd,
+)
+from monai.handlers.utils import from_engine
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.metrics import DiceMetric
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+from monai.data import CacheDataset, DataLoader, Dataset, decollate_batch
+from monai.config import print_config
+from monai.apps import download_and_extract
+import torch
+import matplotlib.pyplot as plt
+import tempfile
+import shutil
+import os
+import glob
+
+def load_unet(batch_size):
+    resource = "https://msd-for-monai.s3-us-west-2.amazonaws.com/Task09_Spleen.tar"
+    md5 = "410d4a301da4e5b2f6f86ec3ddba524e"
+
+    root_dir = os.getcwd()
+    compressed_file = os.path.join(root_dir, "Task09_Spleen.tar")
+    data_dir = os.path.join(root_dir, "Task09_Spleen")
+    if not os.path.exists(data_dir):
+        download_and_extract(resource, compressed_file, root_dir, md5)
+
+    train_images = sorted(glob.glob(os.path.join(data_dir, "imagesTr", "*.nii.gz")))
+    train_labels = sorted(glob.glob(os.path.join(data_dir, "labelsTr", "*.nii.gz")))
+    data_dicts = [{"image": image_name, "label": label_name} for image_name, label_name in zip(train_images, train_labels)]
+    train_files, val_files = data_dicts[:-9], data_dicts[-9:]
+    transform_train = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            ScaleIntensityRanged(
+                keys=["image"],
+                a_min=-57,
+                a_max=164,
+                b_min=0.0,
+                b_max=1.0,
+                clip=True,
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+            RandCropByPosNegLabeld(
+                keys=["image", "label"],
+                label_key="label",
+                spatial_size=(96, 96, 96),
+                pos=1,
+                neg=1,
+                num_samples=4,
+                image_key="image",
+                image_threshold=0,
+            ),
+        ]
+    )
+    transform_test = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            ScaleIntensityRanged(
+                keys=["image"],
+                a_min=-57,
+                a_max=164,
+                b_min=0.0,
+                b_max=1.0,
+                clip=True,
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+        ]
+    )
+
+    train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=4)
+    trainloader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)
+    
+    val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=4)
+    testloader = DataLoader(val_ds, batch_size=1, num_workers=4)
+
+    return trainloader, testloader
+    
+
 def load_cifar10(batch_size):
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
