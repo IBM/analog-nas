64168bd6fbdd0b58adf21e8729ff6f68be52d7da
MatthewSo <matthew.p.v.so@gmail.com>
2024-12-12 17:05:28 -0500

    Adding naive search sample attempt on AutoEncoder config space

--- new_search_sample.py
+++ new_search_sample.py
@@ -1,5 +1,20 @@
 from analogainas.search_spaces.autoencoder.autoencoder_config_space import AutoEncoderConfigSpace
+from analogainas.evaluators.xgboost import XGBoostEvaluator
+from analogainas.search_algorithms.ea_optimized import EAOptimizer
+from analogainas.search_algorithms.worker import Worker
 
 CS = AutoEncoderConfigSpace()
 
 print(CS.hyperparameters)
+
+surrogate = XGBoostEvaluator(model_type="XGBRanker", load_weight=True)  #
+optimizer = EAOptimizer(surrogate, population_size=100, nb_iter=50) # The default population size is 100.
+
+nb_runs = 2
+worker = Worker(CS, optimizer=optimizer, runs=nb_runs)
+
+worker.search()
+worker.result_summary()
+
+best_config = worker.best_config
+best_model = worker.best_arch
