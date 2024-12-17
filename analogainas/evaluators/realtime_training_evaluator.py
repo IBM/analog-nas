a21f7596ca1ffaaf8f94e622c7017ddf2bfa92a8
MatthewSo <matthew.p.v.so@gmail.com>
2024-12-17 01:38:35 -0500

    adding logging

--- analogainas/evaluators/realtime_training_evaluator.py
+++ analogainas/evaluators/realtime_training_evaluator.py
@@ -87,6 +87,7 @@ class RealtimeTrainingEvaluator():
             for i, (inputs, targets) in enumerate(self.train_dataloader):
 
                 inputs, targets = inputs.to(device), targets.to(device)
+                print(f"Inputs: {inputs.shape}, Targets: {targets.shape}")
 
                 outputs = model(inputs)
                 loss = self.criterion(outputs, targets)
