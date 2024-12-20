092e408a188b7fcea45a28bc175282339a21ee96
MatthewSo <matthew.p.v.so@gmail.com>
2024-12-20 10:28:06 -0500

    Cleanup and README

--- cifar_autoencoder_train.py
+++ cifar_autoencoder_train.py
@@ -1,32 +1,15 @@
-import torch
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import numpy as np
 import torch.nn as nn
 
 from analogainas.search_spaces.autoencoder.cifar_autoencoder import CifarAutoEncoder
-from analogainas.search_spaces.config_space import ConfigSpace
 from analogainas.search_spaces.autoencoder.autoencoder_config_space import AutoEncoderConfigSpace
-from analogainas.search_spaces.autoencoder.autoencoder_architecture import AutoEncoder
-from analogainas.search_spaces.autoencoder.mnist_autoencoder import MnistAutoEncoder
-from analogainas.search_spaces.autoencoder.mnist_autoencoder import MnistAutoEncoder
 from analogainas.evaluators.realtime_training_evaluator import RealtimeTrainingEvaluator
 from analogainas.search_algorithms.ea_optimized import EAOptimizer
 from analogainas.search_algorithms.worker import Worker
 from analogainas.search_spaces.dataloaders.autoencoder_structured_dataset import AutoEncoderStructuredDataset
 
-from analogainas.analog_helpers.analog_helpers import create_rpu_config, create_analog_optimizer
-from aihwkit.simulator.configs import InferenceRPUConfig
-from aihwkit.simulator.configs.utils import WeightClipType
-from aihwkit.simulator.configs.utils import BoundManagementType
-from aihwkit.simulator.presets.utils import PresetIOParameters
-from aihwkit.inference.noise.pcm import PCMLikeNoiseModel
-from aihwkit.inference.compensation.drift import GlobalDriftCompensation
-from aihwkit.nn.conversion import convert_to_analog_mapped
-from aihwkit.nn import AnalogSequential
-from aihwkit.optim import AnalogSGD
 
 
 CS = AutoEncoderConfigSpace()
